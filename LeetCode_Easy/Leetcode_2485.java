28b4db0339e0b611cd4947e60e13aac680027530
JoyneelBose02 <joyneelbose16@gmail.com>
2023-05-15 23:10:15 +0530

    Leetcode 2485 Find the pivot integer

---
+++ LeetCode_Easy/Leetcode_2485.java
@@ -0,0 +1,17 @@
+//Leetcode 2485
+//Find the pivot integer
+class Solution {
+    public int pivotInteger(int n) {
+        int fsum=1,bsum=(n*(n+1))/2;
+        
+        if(n==1) return 1;
+        for(int i=2;i<=n;i++)
+        {
+            fsum=fsum+i;
+            bsum=bsum-(i-1);
+            if(fsum==bsum)
+            return i;
+        }
+        return -1;
+    }
+}
