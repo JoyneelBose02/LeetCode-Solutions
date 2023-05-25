bc9ae07510c85bf2bea3203619259497ec81ca04
JoyneelBose02 <joyneelbose16@gmail.com>
2023-05-25 22:20:17 +0530

    Leetcode 1748 Sum of unique elements

---
+++ LeetCode_Easy/Leetcode_1748.java
@@ -0,0 +1,21 @@
+//Leetcode 1748
+//Sum of Unique elements
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int sum = 0;
+        int count;
+        for(int i = 0; i < nums.length; i++) {
+            count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(i != j && nums[i] == nums[j]) {
+                    count++;
+                    break;
+                }
+            }
+            if(count == 0) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+}
