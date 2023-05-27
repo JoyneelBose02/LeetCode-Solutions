db2f115a7528230a58343c66369982c1de0bb2af
JoyneelBose02 <joyneelbose16@gmail.com>
2023-05-27 09:56:31 +0530

    Leetcode 121 Best time to buy and sell stock

---
+++ LeetCode_Easy/Leetcode_121.java
@@ -0,0 +1,14 @@
+//Leetcode 121
+//Best time to Buy and Sell stock
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+        for(int i : prices){
+            min = Math.min(i,min);
+            max = Math.max(max,i-min);
+        }
+        return max;
+        
+    }
+}
