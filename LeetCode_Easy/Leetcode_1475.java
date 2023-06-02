3a9a360c23939cdea272103051a110a0412b32ac
JoyneelBose02 <joyneelbose16@gmail.com>
2023-06-02 20:56:24 +0530

    Leetcode 1475 Final Pricesafter a discount in a shop

---
+++ LeetCode_Easy/Leetcode_1475.java
@@ -0,0 +1,21 @@
+//Leetcode 1475
+//Final Prices after discount in a shop
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int arr[] = new int[prices.length];
+        int i;
+        int j;
+        for(i = 0; i < prices.length; i++) {
+            for(j = i + 1; j < prices.length; j++) {
+                if(prices[j] <= prices[i]) {
+                    arr[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+            if(j == prices.length) {
+                arr[i] = prices[i];
+            }
+        }
+        return arr;
+    }
+}
