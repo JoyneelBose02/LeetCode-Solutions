bf3c65fa4a1d9b857f1941f7ef3e6fa299da1188
JoyneelBose02 <joyneelbose16@gmail.com>
2023-06-14 21:52:57 +0530

    Leetcode 3 Longest Substring without repeating characters

---
+++ LeetCode_Medium/Leetcode_3.java
@@ -0,0 +1,21 @@
+//Leetcode 3
+//Longest Substring without repeating characters
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+            Set<Character>uniq= new HashSet<>();
+            int i=0,j=0,longestSubstringCount= 0;
+            while(j<s.length())
+            {
+              if(!uniq.contains(s.charAt(j)))
+              {
+                uniq.add(s.charAt(j++));
+                longestSubstringCount = Math.max(longestSubstringCount,uniq.size());
+              }
+              else{
+                  uniq.remove(s.charAt(i++));
+              }
+                } 
+     return longestSubstringCount; 
+    }
+}
